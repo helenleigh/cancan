135b261ba0fbbddfb51e601af6a1464ac4376ade
Helen Leigh <helen@doitkits.com>
2019-03-18 23:41:51 +0100

    Create manual_pwm_test.ino
    manual pwm (works on any digital pin)

---
+++ manual_pwm_test/manual_pwm_test.ino
@@ -0,0 +1,45 @@
+void setup()
+{
+pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+//digitalWrite(3, HIGH);
+//delayMicroseconds(100); // 10%
+//digitalWrite(3, LOW);
+//delayMicroseconds(900);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(200); // 20%
+//digitalWrite(3, LOW);
+//delayMicroseconds(800);
+digitalWrite(3, HIGH);
+delayMicroseconds(300); // 30%
+digitalWrite(3, LOW);
+delayMicroseconds(700);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(400); // 40%
+//digitalWrite(3, LOW);
+//delayMicroseconds(600);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(500); // 50%
+//digitalWrite(3, LOW);
+//delayMicroseconds(500);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(600); // 60%
+//digitalWrite(3, LOW);
+//delayMicroseconds(400);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(700); // 70%
+//digitalWrite(3, LOW);
+//delayMicroseconds(300);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(800); // 80%
+//digitalWrite(3, LOW);
+//delayMicroseconds(200);
+//digitalWrite(3, HIGH);
+//delayMicroseconds(900); // 90%
+//digitalWrite(3, LOW);
+//delayMicroseconds(100);
+}
+
